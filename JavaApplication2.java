957ce702e97afbafbf1265da1ec766f5f4869211
731613205024 <731613205024@users.noreply.github.com>
2016-07-16 11:44:54 +0530

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+         Scanner a=new Scanner(System.in);
+        String n=a.nextLine();
+        int b=Integer.parseInt(n);
+        System.out.println(b);
+        
+    }
+}
